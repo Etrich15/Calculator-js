3a556cd06ea7d475f807831d4ad0f950deedb7eb
Etrich15 <etrich@spstrutnov.cz>
2022-04-30 21:58:57 +0200

    halus :

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -13,23 +13,36 @@ exports.loginPost = (req, res) => {
         return res.redirect("/user/login");
     }
     for(let i in usersAll) {
-        if(usersAll[i].name == name && usersAll[i].password == password);
-        console.log(usersAll[i].name);
-        console.log(i);
-        req.session.gypsi = i;
-        req.session.gipsi =  true;
-        return res.redirect("/");
+        if(usersAll[i].name == name && usersAll[i].password == password){
+            console.log(usersAll[i].name);
+            console.log(i);
+            req.session.gypsi = i;
+            req.session.gipsi =  true;
+            return res.redirect("/");
+        }
+        
     }
     res.redirect("/user/login");
 }
 
 exports.register = (req, res) => {
+
     res.render("user/register");
 }
 
 exports.registerPost = (req, res) => {
     const name = req.body.name;
     const password = req.body.password;
+    const usersAll = user.loadUsers();
+    if(password.length == 0 || name.length == 0 || req.session.gipsi) {
+        return res.redirect("/user/register");
+    }
+    for(let i in usersAll){
+        if(usersAll[i].name == name) {
+            return res.redirect("/user/register")
+        }
+    }
+
     user.register(name, password);
     
     res.redirect("/user/login");
